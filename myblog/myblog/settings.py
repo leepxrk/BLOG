506cfc81815539caa852c3d811aac47f65cd2103
leepxrk <leepx01rk@hotmail.com>
2017-10-18 13:53:34 +0800

    1
    1

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'root',
+        'PASSWORD': '19900807',
+        'HOST': '',
+        'PORT': '3306'
     }
 }
 
