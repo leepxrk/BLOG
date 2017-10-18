70d318e1b929ef8f08b4309ced20fa63b3a9d858
leepxrk <leepx01rk@hotmail.com>
2017-10-18 14:01:03 +0800

    2
    2

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'myblog'),
         'USER': 'root',
         'PASSWORD': '19900807',
         'HOST': '',
