9979654d26eca801ea374f345660049ce6d937c4
leepxrk <leepx01rk@hotmail.com>
2017-10-22 14:18:21 +0800

    12
    12

--- myblog/polls/models.py
+++ myblog/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Qusetion(models.Model):
+    qusetion_text = models.Charfield(max_length = 200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    qusetion = models.Foreignkey(Qusetion)
+    Choice_text = models.Charfield(max_length = 200)
+    votes = models.IntegerField(default = 0)
